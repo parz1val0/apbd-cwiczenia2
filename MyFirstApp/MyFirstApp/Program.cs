bb544705cc522a6aa0f937bd2503b5244635444f
parz1val0 <dema040605@gmail.com>
2024-03-08 13:37:58 +0100

    add method get avarage

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -4,3 +4,18 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Pierwsza Zmiana");
 Console.WriteLine("Druga zmiania");
 Console.WriteLine("3 zmiania");
+
+
+static double GetAvarage(int[] numbers)
+{
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+    return (double) sum/numbers.Length;
+}
+
+/*
+int[] numbers = { 1, 2, 3, 4 };
+double avg = GetAvarage(numbers);*/
