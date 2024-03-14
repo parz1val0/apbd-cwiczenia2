53ffc38d525673d0c9d12ccfce86f203e4307de2
parz1val0 <dema040605@gmail.com>
2024-03-14 20:15:36 +0100

    Add counting max mqthod and on the console

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -15,7 +15,20 @@ static double GetAvarage(int[] numbers)
     }
     return (double) sum/numbers.Length;
 }
+static int GetMax(int[] numbers)
+{
+    int max = 0;
+    foreach (var number in numbers)
+    {
+        if (max < number)
+        {
+            max = number;
+        }
+    }
+    return max;
+}
 
 
 int[] numbers = { 1, 2, 3, 4 };
+Console.WriteLine(GetMax(numbers));
 double avg = GetAvarage(numbers);
